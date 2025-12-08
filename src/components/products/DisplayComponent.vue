00d636ff2d4eab56f1fe054fef4dd9fa58a5fd11
Joachem Trinidad <jtrinidad@vauldex.com>
2025-12-08 11:49:35 +0800

    feat: add filter products

--- src/components/products/DisplayComponent.vue
+++ src/components/products/DisplayComponent.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="product-display show">
-        <div class="product-grid"><div 
-            v-for="product in productStore.products" 
-            :key="product.id" 
-            class="product-card"
-            @click="goToProduct(product)"
-        >
+        <div class="search-bar">
+            <h2>Products</h2>
+            <input 
+                v-model="searchQuery" 
+                placeholder="Search..." 
+                @keyup.enter="filterProducts"
+            />
+        </div>
+
+        <div class="product-grid">
+            <div 
+                v-for="product in filteredProducts" 
+                :key="product.id" 
+                class="product-card"
+                @click="goToProduct(product)"
+            >
                 <div class="image-wrapper">
                     <img 
                         :src="productImages[`../../assets/products/${product.name}.png`]" 
@@ -31,21 +41,29 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useProductStore } from '../../store/product.store'
 import type { ProductResponse } from '../../types/product'
 import { useRouter } from 'vue-router'
 import { addToCart } from '../../services/cart'
 import { toast } from 'vue3-toastify'
 
-
 const router = useRouter()
 const productStore = useProductStore()
+const searchQuery = ref('')
 
 onMounted(async () => {
     await productStore.getAllProducts()
 })
+
 const productImages = import.meta.glob('../../assets/products/*.png', { eager: true, as: 'url' })
+
+const filteredProducts = computed(() => {
+    if (!searchQuery.value) return productStore.products
+    return productStore.products.filter(p => 
+        p.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+    )
+})
 
 function goToProduct(product: ProductResponse) {
     productStore.selectedProduct = product
@@ -57,13 +75,19 @@ function addProductToCart(product: ProductResponse) {
     toast.success("Added to cart")
 }
 
-
+function filterProducts() {
+    if (!searchQuery.value) return productStore.products
+    return productStore.products.filter(p => 
+        p.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+    )
+} 
 
 </script>
 
 <style scoped>
 .product-display {
     padding: 2vh;
+    min-height: 91.5vh;
 }
 
 .product-grid {
@@ -119,5 +143,33 @@ function addProductToCart(product: ProductResponse) {
 
 .product-info h3 {
     margin: 0.5vh 0;
+}
+
+
+.search-bar {
+    display: flex;
+    margin-bottom: 2vh;  
+    align-items: center;
+}
+
+.search-bar h2{
+    margin: 0;
+}
+
+.search-icon{
+    height: 1.5vw;
+}
+.search-bar input { 
+    padding: 0.5vh 1vh;
+    font-size: 0.9rem;
+    width: 20%;
+    height: 2vh;
+    margin-left: auto;
+}
+
+.search-bar button {
+    padding: 0.5vh 1vh;
+    margin-left: 0.5vh;
+    cursor: pointer;
 }
 </style>
